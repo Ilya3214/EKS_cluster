9abb520d4bc307c774c1ba96d35165bb7d1e75c8
Ilya3214 <ilpredko@gmail.com>
2024-05-13 15:53:05 -0500

    deploy cluster

--- root/project-x/main.tf
+++ root/project-x/main.tf
@@ -8,36 +8,35 @@ module "vpc" {
   vpc_cidr           = var.vpc_cidr
 }
 
-# module "eks-cluster" {
-#   source       = "../../eks-module"
-#   cluster_name = var.cluster_name
+module "eks-cluster" {
+  source       = "../../eks-module"
+  cluster_name = var.cluster_name
 
-#   # AWS Auth
-#   administrator_role_name = var.administrator_role_name
+  # AWS Auth
+  administrator_role_name = var.administrator_role_name
 
-#   # VPC
-#   vpc_id     = module.vpc.vpc_id
-#   subnet_ids = module.vpc.public_subnet_ids
+  # VPC
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.public_subnet_ids
 
-#   # EKS
-#   cluster_version  = var.cluster_version
-#   k8s_service_cidr = var.k8s_service_cidr
+  # EKS
+  cluster_version  = var.cluster_version
+  k8s_service_cidr = var.k8s_service_cidr
 
-#   # Worker
-#   desired_capacity = var.desired_capacity
-#   instance_type    = var.instance_type
-#   max_size         = var.max_size
-#   min_size         = var.min_size
+  # Worker
+  desired_capacity = var.desired_capacity
+  instance_type    = var.instance_type
+  max_size         = var.max_size
+  min_size         = var.min_size
 
-#   # Role for GitHub CI
-#   gitHubActionsAppCIRoleDev     = var.gitHubActionsAppCIRoleDev
-#   gitHubActionsAppCIRoleStaging = var.gitHubActionsAppCIRoleStaging
-#   gitHubActionsAppCIRoleProd    = var.gitHubActionsAppCIRoleProd
+  # Role for GitHub CI
+  gitHubActionsAppCIRoleDev     = var.gitHubActionsAppCIRoleDev
 
-# }
 
-#module "iam_roles" {
-#  source                  = "../../iam-module"
-#  specific_user_arn       = var.specific_user_arn
-#  administrator_role_name = var.administrator_role_name
-#}
+}
+
+module "iam_roles" {
+ source                  = "../../iam-module"
+ specific_user_arn       = var.specific_user_arn
+ administrator_role_name = var.administrator_role_name
+}
