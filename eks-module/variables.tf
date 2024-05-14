1fc42b752f4e45b2baa12c9946fe3d491dc102ab
Ilya3214 <ilpredko@gmail.com>
2024-05-14 12:21:41 -0500

    test node group

--- eks-module/variables.tf
+++ eks-module/variables.tf
@@ -1,15 +1,102 @@
+# variable "cluster_name" {
+#   description = "The name of a project"
+#   type        = string
+# }
+
+# # AWS AUTH
+# variable "administrator_role_name" {
+#   description = "The name of the IAM role for the administrator."
+#   type        = string
+# }
+
+# # EKS CLUSTER
+# variable "vpc_id" {
+#   type        = string
+#   description = "VPC to launch EKS cluster and worker nodes"
+# }
+
+# variable "subnet_ids" {
+#   type        = list(string)
+#   description = "Subnet IDs to launch EKS cluster and worker nodes"
+# }
+
+# variable "cluster_version" {
+#   description = "The desired Kubernetes version for the EKS cluster"
+#   type        = string
+# }
+
+# variable "k8s_service_cidr" {
+#   description = "The service IPv4 CIDR for the Kubernetes cluster"
+#   type        = string
+# }
+
+
+# # WORKER NODES
+
+# variable "instance_type" {
+#   description = "Instance type for the EKS worker nodes"
+#   type        = string
+# }
+
+# variable "desired_capacity" {
+#   description = "Desired number of instances in the EKS worker nodes auto-scaling group"
+#   type        = number
+# }
+
+# variable "max_size" {
+#   description = "Maximum number of instances in the EKS worker nodes auto-scaling group"
+#   type        = number
+# }
+
+# variable "min_size" {
+#   description = "Minimum number of instances in the EKS worker nodes auto-scaling group"
+#   type        = number
+# }
+
+# variable "eks_role_policy_arns" {
+#   description = "Policy ARNs for EKS Cluster Role"
+#   type        = list(string)
+#   default = [
+#     "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
+#     "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController",
+#     "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+#   ]
+# }
+
+# variable "worker_role_policy_arns" {
+#   description = "Policy ARNs for EKS Worker Role"
+#   type        = list(string)
+#   default = [
+#     "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+#     "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+#     "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+#   ]
+# }
+
+# variable "gitHubActionsAppCIRoleDev" {
+#   description = "Dev role that allow to GitHub actions to create resources inside k8s"
+#   type        = string
+# }
+
+# variable "gitHubActionsAppCIRoleStaging" {
+#   description = "Staging role that allow to GitHub actions to create resources inside k8s"
+#   type        = string
+# }
+
+# variable "gitHubActionsAppCIRoleProd" {
+#   description = "Prod role that allow to GitHub actions to create resources inside k8s"
+#   type        = string
+# }
+
+
+
+#############     Config for node group     #############
+
 variable "cluster_name" {
   description = "The name of a project"
   type        = string
 }
 
-# AWS AUTH
-variable "administrator_role_name" {
-  description = "The name of the IAM role for the administrator."
-  type        = string
-}
-
-# EKS CLUSTER
 variable "vpc_id" {
   type        = string
   description = "VPC to launch EKS cluster and worker nodes"
@@ -29,9 +116,6 @@ variable "k8s_service_cidr" {
   description = "The service IPv4 CIDR for the Kubernetes cluster"
   type        = string
 }
-
-
-# WORKER NODES
 
 variable "instance_type" {
   description = "Instance type for the EKS worker nodes"
@@ -78,12 +162,8 @@ variable "gitHubActionsAppCIRoleDev" {
   type        = string
 }
 
-# variable "gitHubActionsAppCIRoleStaging" {
-#   description = "Staging role that allow to GitHub actions to create resources inside k8s"
-#   type        = string
-# }
+variable "ec2_ssh_key_name" {
+  description = "Name of the SSH key to use for the instances in the Node Group"
+  type        = string
+}
 
-# variable "gitHubActionsAppCIRoleProd" {
-#   description = "Prod role that allow to GitHub actions to create resources inside k8s"
-#   type        = string
-# }
--- root/project-x/main.tf
+++ root/project-x/main.tf
@@ -1,11 +1,61 @@
-module "vpc" {
-  source = "../../vpc-module"
+# module "vpc" {
+#   source = "../../vpc-module"
 
-  availability_zones = var.availability_zones
-  private_subnets    = var.private_subnets
-  cluster_name       = var.cluster_name
-  public_subnets     = var.public_subnets
-  vpc_cidr           = var.vpc_cidr
+#   availability_zones = var.availability_zones
+#   private_subnets    = var.private_subnets
+#   cluster_name       = var.cluster_name
+#   public_subnets     = var.public_subnets
+#   vpc_cidr           = var.vpc_cidr
+# }
+
+# module "eks-cluster" {
+#   source       = "../../eks-module"
+#   cluster_name = var.cluster_name
+
+#   # AWS Auth
+#   administrator_role_name = var.administrator_role_name
+
+#   # VPC
+#   vpc_id     = module.vpc.vpc_id
+#   subnet_ids = module.vpc.public_subnet_ids
+
+#   # EKS
+#   cluster_version  = var.cluster_version
+#   k8s_service_cidr = var.k8s_service_cidr
+
+#   # Worker
+#   desired_capacity = var.desired_capacity
+#   instance_type    = var.instance_type
+#   max_size         = var.max_size
+#   min_size         = var.min_size
+
+#   # Role for GitHub CI
+#   gitHubActionsAppCIRoleDev     = var.gitHubActionsAppCIRoleDev
+
+
+# }
+
+# module "iam_roles" {
+#  source                  = "../../iam-module"
+#  specific_user_arn       = var.specific_user_arn
+#  administrator_role_name = var.administrator_role_name
+# }
+
+
+
+########
+
+provider "aws" {
+  region = var.region
+}
+
+module "vpc" {
+  source              = "../../vpc-module"
+  cluster_name        = var.cluster_name
+  availability_zones  = var.availability_zones
+  private_subnets     = var.private_subnets
+  public_subnets      = var.public_subnets
+  vpc_cidr            = var.vpc_cidr
 }
 
 module "eks-cluster" {
@@ -28,17 +78,14 @@ module "eks-cluster" {
   instance_type    = var.instance_type
   max_size         = var.max_size
   min_size         = var.min_size
+  ec2_ssh_key_name = var.ec2_ssh_key_name
 
   # Role for GitHub CI
-  gitHubActionsAppCIRoleDev     = var.gitHubActionsAppCIRoleDev
-
-
+  gitHubActionsAppCIRoleDev = var.gitHubActionsAppCIRoleDev
 }
 
 module "iam_roles" {
- source                  = "../../iam-module"
- specific_user_arn       = var.specific_user_arn
- administrator_role_name = var.administrator_role_name
+  source                  = "../../iam-module"
+  specific_user_arn       = var.specific_user_arn
+  administrator_role_name = var.administrator_role_name
 }
-
-   
