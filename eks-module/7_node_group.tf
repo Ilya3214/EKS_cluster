b1ac162f4a1cdffdd41dde2b4bb17358d4409765
Ilya3214 <ilpredko@gmail.com>
2024-05-14 13:19:16 -0500

    test1

--- eks-module/7_node_group.tf
+++ eks-module/7_node_group.tf
@@ -1,22 +1,22 @@
-# resource "aws_eks_node_group" "eks_node_group" {
-#   cluster_name    = aws_eks_cluster.eks_cluster.name
-#   node_group_name = "${var.cluster_name}-node-group"
-#   node_role_arn   = aws_iam_role.eks_worker_role.arn
-#   subnet_ids      = var.subnet_ids
+resource "aws_eks_node_group" "eks_node_group" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "${var.cluster_name}-node-group"
+  node_role_arn   = aws_iam_role.eks_worker_role.arn
+  subnet_ids      = var.subnet_ids
 
-#   scaling_config {
-#     desired_size = var.desired_capacity
-#     max_size     = var.max_size
-#     min_size     = var.min_size
-#   }
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
 
-#   instance_types = [var.instance_type]
+  instance_types = [var.instance_type]
 
-#   remote_access {
-#     ec2_ssh_key = var.ec2_ssh_key_name
-#   }
+  remote_access {
+    ec2_ssh_key = "linuxkey.pem"
+  }
 
-#   tags = {
-#     "Name" = "${var.cluster_name}-node-group"
-#   }
-# }
+  tags = {
+    "Name" = "${var.cluster_name}-node-group"
+  }
+}
